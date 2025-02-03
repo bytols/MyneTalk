0de96e86b8f56f8e63fcd9f6a5b3d17e3c173a6a
Erick <epickbr123@gmail.com>
2025-02-03 20:19:04 -0300

    comecando tratamento de letras

--- client.c
+++ client.c
@@ -1,10 +1,33 @@
 #include "minitalk.h"
+void send_letter(char *str)
+{
+    int i;
+    int bin_num;
+    
+    i = 0;
+    while(str[i])
+    {
+        bin_num = 1;
+        while(bin_num <= 128)
+        {
+            if((str[i] & bin_num) == bin_num)
+                ft_printf("um \n");
+            else
+                ft_printf("zero \n");
+            bin_num = bin_num + bin_num;
+        }
+        i++;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     int pid;
 
-    (void) argc;
+    if(argc != 3)
+        return (0);
+    send_letter(argv[2]);
     pid = ft_atoi(argv[1]);
-    kill(SIGINT, pid);
-    ft_printf("enviado\n", pid);
+    ft_printf("aqui o pid %d\n", pid);
+    kill(pid, SIGINT);
 }
--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: erocha-l <erocha-l@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 19:54:34 by erocha-l          #+#    #+#             */
-/*   Updated: 2025/02/01 00:20:20 by erocha-l         ###   ########.fr       */
+/*   Updated: 2025/02/03 14:49:03 by erocha-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,12 @@
 # include <stdio.h>
 # include <stdarg.h>
 
+typedef struct letra 
+{
+    char letra;
+    int size;
+} letter ; 
+
 int			ft_printf(const char *format, ...);
 void		ft_putb(unsigned int n, int fd, char *base, int *count);
 void		ft_putchar_fd(char c, int fd, int *count);
@@ -26,6 +32,5 @@ void		ft_putstr_fd(char *c, int fd, int *count);
 void		ft_putunbr_fd(unsigned int n, int fd, int *count);
 void	convert_ptr(void *ptr, int *count);
 int			ft_atoi(const char *nptr);
-
 
 #endif
